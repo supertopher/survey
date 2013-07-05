158deba3455a75c695706110033ae5f31cf9dbe8
supertopher <asimplechris@gmail.com>
2013-07-05 11:06:03 -0700

    Added migrations to db

--- db/migrate/20130705103929_create_responses.rb
+++ db/migrate/20130705103929_create_responses.rb
@@ -1,4 +1,9 @@
 class CreateResponses < ActiveRecord::Migration
   def change
+    create_table :responses do |t|
+      t.string :response
+      t.belongs_to :user
+      t.belongs_to :question
+    end
   end
 end

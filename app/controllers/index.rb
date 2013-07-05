f3285d35c9e150ac69bbcb81b45a0434c7ebb562
Apprentice <apprentice@devbootcamp.com>
2013-07-05 12:13:07 -0700

    Commit for pull

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,36 +2,3 @@ get "/" do
   erb :index
 end
 
-get "/create" do
-  erb :create_user
-end
-
-
-post '/create' do
-  puts params
-  user = User.create(params[:user])
-  if user
-    session[:user] = user.id
-  end
-  redirect "/"
-end
-
-get '/logout' do
-
-  session.clear
-  redirect '/'
-
-end
-
-get "/login" do
-  erb :login
-end
-
-post "/login" do 
-  user = User.find_by_user_name(params[:user][:user_name])
-  authenticate = User.authenticate(params[:user])
-  if authenticate
-    session[:user] = user.id
-  end
-  redirect "/"
-end
